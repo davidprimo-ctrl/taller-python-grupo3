c2bcebec20abc60d6643f2225924267aa1bf2df1
Sergios <sergio.gutierrez@estudiantesunibague.edu.com>
2026-08-27 17:41:30 -0500

    Operacion

--- Resta.py
+++ Resta.py
@@ -1,6 +1,3 @@
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
-
-resultado = num1 - num2
-
-print("El resultado es:", resultado)
+def resta(num1, num2):
+    resultado = num1 - num2
+    return resultado
